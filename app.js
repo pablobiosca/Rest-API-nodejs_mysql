f50b6cae68b797cf5bf059b7916950a6daf5f76b
chabaking <zaslake@gmail.com>
2023-01-25 13:36:26 +0100

    lista para produccion

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv = require("dotenv")
+dotenv.config()
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
 
 const app = express();
 
@@ -15,6 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,22 @@ const router = express.Router();
 const {pool} = require("../db.js")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const [rows] = await pool.promise().query("select * from users")
+
+  res.json(rows)
 });
 
+
+
 router.get('/ping', async function(req, res, next) {
-  const result =  await pool.promise().query("select 'hello wordl' as result")
+  const result =  await pool.promise().query("select * from users")
+  console.log(result[0])
+  res.json(result[0])
+});
+
+router.get('/create', async function(req, res, next) {
+  const result =  await pool.promise().query("INSERT INTO USERS(NAME) VALUES('kitty')")
   console.log(result[0])
   res.json(result[0])
 });
